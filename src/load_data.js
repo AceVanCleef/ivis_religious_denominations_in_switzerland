6064ab3e862d4e089c132a7b766f9fb20bdafc2f
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2018-05-08 16:34:48 +0200

    tried to implement csv import

--- src/load_data.js
+++ src/load_data.js
@@ -1,3 +1,29 @@
 export function printtest(){
 	console.log("hi");
 }
+
+export function loadReligionData(){
+	d3.csv("../data/religions.csv", function(error, d) {
+		//accessor function: offers full control on how you return each item.
+		console.log(d[0]);
+	  return {
+	  	//kanton;jahr;total;reformierte;katholiken;andere-christen;juden;islamisten;andere-religionen;konfessionslose
+	    	kanton 				: d.kanton,
+	    	jahr				: +d.jahr,
+	    	total				: +d.total,
+	    	reformierte			: +d.reformierte,
+	    	katholiken			: +d.katholiken,
+	    	andereChristen		: +d.andere-christen,
+	    	juden				: +d.juden,
+	    	islamisten			: +d.islamisten,
+	    	andereReligionen	: +d.andere-religionen,
+	    	konfessionslose		: +d.konfessionslose
+	  };
+	}, function(data) {
+	  //call functions to process your data here.
+		console.log("processed data:")
+		console.log(data.jahr);
+		console.log(data);
+	    return data;
+	});
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import {printtest} from "/src/load_data.js";
+import {loadReligionData, printtest} from "/src/load_data.js";
 
 
 
 const main = function() {
 	console.log("hello");
 	printtest();
+	const rawReligionData = loadReligionData();
+	console.log(rawReligionData);
 }
 
 main();
