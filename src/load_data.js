c9c5aaa9a764f2de7ba5d782f4d4f06233ed8537
Mario Winiker <mario.winiker@gmail.com>
2018-05-10 17:06:36 +0200

    Change most data-types from string to number when importing

--- src/load_data.js
+++ src/load_data.js
@@ -1,15 +1,15 @@
 d3.csv("/data/religions.csv", (d) => {
     return {
         kanton : d.kanton,
-        jahr : d.jahr,
-        total : d.total,
-        reformierte : d.reformierte,
-        katholiken : d.katholiken,
-        andere_christen : d.andere_christen,
-        juden : d.juden,
-        islamisten : d.islamisten,
-        andere_religionen : d.andere_religionen,
-        konfessionslose : d.konfessionslose
+        jahr : +d.jahr,
+        total : +d.total,
+        reformierte : +d.reformierte,
+        katholiken : +d.katholiken,
+        andere_christen : +d.andere_christen,
+        juden : +d.juden,
+        islamisten : +d.islamisten,
+        andere_religionen : +d.andere_religionen,
+        konfessionslose : +d.konfessionslose
     };
 }, (data) => {
     console.log(data);
